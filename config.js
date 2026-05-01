b0336411387426a8bc54a505728401d098e9166a
examtestgenius-create <examtestgenius@gmail.com>
2026-05-01 19:03:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // StudyHub Frontend Config (Go-Live)
 // Paste your deployed Apps Script Web App URL (ends with /exec)
 window.STUDYHUB_CONFIG = window.STUDYHUB_CONFIG || {};
-window.STUDYHUB_CONFIG.webappUrl = '__PASTE_APPS_SCRIPT_WEBAPP_URL__';
+window.STUDYHUB_CONFIG.webappUrl = 'https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec';
 window.STUDYHUB_CONFIG.siteBaseUrl = '__PASTE_GITHUB_PAGES_BASE_URL__';
 window.STUDYHUB_CONFIG.liveCatalogUrl = window.STUDYHUB_CONFIG.webappUrl ? (window.STUDYHUB_CONFIG.webappUrl + '?action=catalog') : '';
 window.STUDYHUB_CONFIG.apiBaseUrl = window.STUDYHUB_CONFIG.webappUrl || '';
