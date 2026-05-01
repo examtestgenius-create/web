7d61c76dd60eefeb07371160721d994342eaddd2
examtestgenius-create <examtestgenius@gmail.com>
2026-05-01 20:25:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 window.STUDYHUB_CONFIG = {
   // ✅ ONLY valid Apps Script Web App endpoint
-  webappUrl: "https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec",
+  webappUrl: "https://script.google.com/macros/s/AKfycbyE_IJKWqFcOIoAIJ1I7yOAZ1n5E8PLQt-BLJeDgfgZ6wu15wv3e-1YqmJsRzA4skrf/exec",
 
   // ✅ Live site
   siteBaseUrl: "https://examtestpaper.co.za",
