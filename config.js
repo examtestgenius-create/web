eec004c2e63d7ce221bc7e8a87aaa6801f50066e
examtestgenius-create <examtestgenius@gmail.com>
2026-05-01 18:59:16 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.STUDYHUB_CONFIG = window.STUDYHUB_CONFIG || {};
-window.STUDYHUB_CONFIG.webappUrl = 'https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec';
-window.STUDYHUB_CONFIG.liveCatalogUrl = window.STUDYHUB_CONFIG.webappUrl + '?action=catalog';
-window.STUDYHUB_CONFIG.fallbackCatalogUrl = 'data/catalog.sample.json';
