12aefddc117fa88d0484ef63f5b374812e41ae11
examtestgenius-create <examtestgenius@gmail.com>
2026-05-02 21:06:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // siteBaseUrl should be your GitHub Pages base URL (no trailing slash)
 window.STUDYHUB_CONFIG = window.STUDYHUB_CONFIG || {};
 window.STUDYHUB_CONFIG.webappUrl = 'https://script.google.com/macros/s/AKfycbyrGxdt2HhRklPLlpYq_1P-dGh1NfkXAXw-ZJkHmWZ5SJy5vCOHuSakr4LBXwqgz0gV8Q/exec';
-window.STUDYHUB_CONFIG.siteBaseUrl = '__PASTE_GITHUB_PAGES_BASE_URL__';
+window.STUDYHUB_CONFIG.siteBaseUrl = 'https://examtestpaper.co.za';
 window.STUDYHUB_CONFIG.liveCatalogUrl = window.STUDYHUB_CONFIG.webappUrl ? (window.STUDYHUB_CONFIG.webappUrl + '?action=catalog') : '';
 window.STUDYHUB_CONFIG.apiBaseUrl = window.STUDYHUB_CONFIG.webappUrl || '';
 window.STUDYHUB_CONFIG.fallbackCatalogUrl = 'data/catalog.sample.json';
