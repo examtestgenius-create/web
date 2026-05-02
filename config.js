ec2b1b1540b215abd92089150902fb4deb17d6d7
examtestgenius-create <examtestgenius@gmail.com>
2026-05-02 20:36:31 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// ExamTestPaper / StudyHub Frontend Config (LIVE – FIXED)
-
-window.STUDYHUB_CONFIG = {
-  webappUrl: 'https://script.google.com/macros/s/AKfycbyrGxdt2HhRklPLlpYq_1P-dGh1NfkXAXw-ZJkHmWZ5SJy5vCOHuSakr4LBXwqgz0gV8Q/exec',
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyrGxdt2HhRklPLlpYq_1P-dGh1NfkXAXw-ZJkHmWZ5SJy5vCOHuSakr4LBXwqgz0gV8Q/exec',
-  liveCatalogUrl: 'https://script.google.com/macros/s/AKfycbyrGxdt2HhRklPLlpYq_1P-dGh1NfkXAXw-ZJkHmWZ5SJy5vCOHuSakr4LBXwqgz0gV8Q/exec?action=catalog',
-  siteBaseUrl: 'https://examtestpaper.co.za',
-  fallbackCatalogUrl: 'data/catalog.sample.json'
-};
