eb93a8d92e320bb4c607cbe9409e980778da9053
examtestgenius-create <examtestgenius@gmail.com>
2026-05-03 20:26:09 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.STUDYHUB_CONFIG = {
   payfast: {
-    merchant_id: "12345678", // digits only (PayFast requires integer) 
-    merchant_key: "XXXXXXXXXXXXX", // 13 chars as PayFast validates
+    merchant_id: "33250069", // digits only (PayFast requires integer) 
+    merchant_key: "1qwkh8wrtemwh", // 13 chars as PayFast validates
     return_url: "https://YOUR_USERNAME.github.io/studyhub-site/success.html",
     cancel_url: "https://YOUR_USERNAME.github.io/studyhub-site/catalog.html",
     notify_url: "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec"
