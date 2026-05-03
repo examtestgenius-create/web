ea74d609b2242b6ae7687b57e9547677d6764bed
examtestgenius-create <examtestgenius@gmail.com>
2026-05-03 20:27:35 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,6 @@ window.STUDYHUB_CONFIG = {
     merchant_key: "1qwkh8wrtemwh", // 13 chars as PayFast validates
     return_url: "https://YOUR_USERNAME.github.io/studyhub-site/success.html",
     cancel_url: "https://YOUR_USERNAME.github.io/studyhub-site/catalog.html",
-    notify_url: "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec"
+    notify_url: "https://script.google.com/macros/s/1zr71luvg_FXOvn2wVq97wXlSfYmmIc9AD3IpiHvBPbvECtGUk7z3ULth/exec"
   }
 };
