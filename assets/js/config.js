059165c761d70b6016d6df3032277af5dd9a745f
examtestgenius-create <examtestgenius@gmail.com>
2026-05-04 12:46:42 +0200

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,9 +0,0 @@
-window.STUDYHUB_CONFIG = {
-  payfast: {
-    merchant_id: "33250069", // digits only (PayFast requires integer) 
-    merchant_key: "1qwkh8wrtemwh", // 13 chars as PayFast validates
-    return_url: "https://YOUR_USERNAME.github.io/studyhub-site/success.html",
-    cancel_url: "https://YOUR_USERNAME.github.io/studyhub-site/catalog.html",
-    notify_url: "https://script.google.com/macros/s/1zr71luvg_FXOvn2wVq97wXlSfYmmIc9AD3IpiHvBPbvECtGUk7z3ULth/exec"
-  }
-};
