eef06b774ba62efadfde73869981854edbcd293a
examtestgenius-create <examtestgenius@gmail.com>
2026-05-17 16:01:58 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// StudyHub production config - v4.2
-window.STUDYHUB_CONFIG = {
-  webappUrl: 'https://script.google.com/macros/s/AKfycbyXt4B0uC9UBVQil7DZN_QbxasaiE0BJAU82mdaXGRMtTwzkpJaZdpSOQAPgzo-A1Cu/exec',
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyXt4B0uC9UBVQil7DZN_QbxasaiE0BJAU82mdaXGRMtTwzkpJaZdpSOQAPgzo-A1Cu/exec',
-  liveCatalogUrl: 'https://script.google.com/macros/s/AKfycbyXt4B0uC9UBVQil7DZN_QbxasaiE0BJAU82mdaXGRMtTwzkpJaZdpSOQAPgzo-A1Cu/exec?action=catalog',
-  siteBaseUrl: 'https://examtestpaper.co.za',
-  fallbackCatalogUrl: 'data/catalog.sample.json',
-  currency: 'ZAR'
-};
