729500efb9a5602af407d3ba1cf037ddd19cd5e4
examtestgenius-create <examtestgenius@gmail.com>
2026-09-04 09:33:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-window.STUDYHUB_CONFIG={siteBaseUrl:'https://examtestpaper.co.za',apiBaseUrl:'https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec',webappUrl:'https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec',liveCatalogUrl:'https://script.google.com/macros/s/AKfycbzP1FMuDC92FKnQ0T-DJrMah3b7vsO1rADn4IYq5kjXzu79C4hdu1fyo1vGhxs9K5Vy/exec?api=catalogPublic',fallbackCatalogUrl:'',contactEmail:'examtestgenius@gmail.com'};
+window.STUDYHUB_CONFIG = {
+  siteBaseUrl: 'https://examtestpaper.co.za',
+
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyaL0g4sF4TWLRjSR1ioLV7J01msJejXMzgSttJzgQr_upEKXkx_tGagwXnLjMuT1bl_A/exec',
+
+  webappUrl: 'https://script.google.com/macros/s/AKfycbyaL0g4sF4TWLRjSR1ioLV7J01msJejXMzgSttJzgQr_upEKXkx_tGagwXnLjMuT1bl_A/exec',
+
+  liveCatalogUrl: 'https://script.google.com/macros/s/AKfycbyaL0g4sF4TWLRjSR1ioLV7J01msJejXMzgSttJzgQr_upEKXkx_tGagwXnLjMuT1bl_A/exec?api=catalogPublic',
+
+  fallbackCatalogUrl: '',
+
+  contactEmail: 'examtestgenius@gmail.com'
+};
